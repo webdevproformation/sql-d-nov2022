c1061f0c15d1ca1a4146034c8f151b2385b28087
Malik H <malik.h@webdevpro.net>
2022-11-03 16:38:56 +0100

    correction

--- jour2/06-join.sql
+++ jour2/06-join.sql
@@ -77,8 +77,21 @@ créer une table catégorie
 clé primaire
 nom 
 
-Orientale
-Méditerranéenne
+CREATE TABLE categorie(
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ nom VARCHAR(100)
+);
+
+INSERT INTO categorie
+( nom )
+VALUES 
+("Orientale" ) , 
+( "Méditerranéenne");
+
+/* Orientale
+Méditerranéenne */
+
+
 
 créer une table recette 
 clé primaire
@@ -87,13 +100,41 @@ prix
 id_categorie
 id_author 
 
+CREATE TABLE recette (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom VARCHAR(100),
+    prix FLOAT ,
+    id_categorie INTEGER ,
+    id_author INTEGER ,
+    FOREIGN KEY (id_categorie) REFERENCES categorie(id),
+    FOREIGN KEY (id_author) REFERENCES author(id)
+);
+
+PRAGMA foreign_keys = ON;
+
+INSERT INTO recette 
+(nom , prix , id_categorie , id_author)
+VALUES
+("frite" , 5 , 2 , 1),
+("blanquette de veau" , 15 , 1 , 4);
+
+
 "frite"                 5   associé à catégorie Méditerranéenne et l'auteur La Fontaine
 "blanquette de veau"   15   associé à catégorie Orientale et l'auteur George Sand 
 
 => réaliser une requête dans laquelle vous allez afficher
 nom recette / nom catégorie / prix recette / nom de l'auteur 
 
-
+SELECT 
+    recette.nom AS `nom de la recette` , 
+    categorie.nom AS `catégorie associée` ,
+    recette.prix , 
+    author.nom  AS `nom créateur`
+FROM recette
+JOIN categorie
+ON recette.id_categorie = categorie.id 
+JOIN author 
+ON recette.id_author = author.id ; 
 
 
 
