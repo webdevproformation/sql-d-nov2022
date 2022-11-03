0a1bf71ed02060e805104da766502b3ef710f0b7
Malik H <malik.h@webdevpro.net>
2022-11-03 15:52:41 +0100

    cas pratique

--- jour2/06-join.sql
+++ jour2/06-join.sql
@@ -24,3 +24,22 @@ SELECT nom , prix , url
 FROM  image
 JOIN  produit /* la table de jointure où sont stockées les données */
 ON produit.id = image.id_produit ; /* Quelle est la colonne de liaison entre les deux tables */
+
+
+-- ajouter les valeurs suivantes dans les tables suivantes :
+
+author 
+ "La Fontaine" "Philosophe"
+ "Victor Hugo"  "Ecrivain"
+ "George Sand"  "Ecrivain"
+
+post 
+"Les Misérables" 1800-01-01   => associé à Victor Hugo
+"Notre Dame" 1800-02-01   => associé à Victor Hugo
+"la Mare au Diable" 1800-02-01   => associé à George Sand
+"Les Fables de la Fontaine" 1800-02-01   => associé à La Fontaine
+
+
+=> réaliser une requête SELECT 
+
+nom auteur / nom du livre / dt publication du livre 
